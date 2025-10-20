7d06b84a8918471a56f6fdee681a5f27d4f42220
Sofaxo Web Services <alihaider4938495@gmail.com>
2025-10-20 12:59:35 +0500

    inculde header , footer

---
+++ include.js
@@ -0,0 +1,16 @@
+// include.js
+document.addEventListener("DOMContentLoaded", () => {
+  // Load header
+  fetch("header.html")
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById("header").innerHTML = data;
+    });
+
+  // Load footer
+  fetch("footer.html")
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById("footer").innerHTML = data;
+    });
+});
